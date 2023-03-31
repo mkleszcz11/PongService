2032322a663c1434ab00d43faa32303653d576cf
Blazej <blazejfurgala@outlook.com>
2023-03-31 12:34:38 +0200

    Add project files.

---
+++ DeviceEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongService
+{
+    public enum DeviceEnum
+    {
+        VoiceModule = 1,
+        TangibleModule = 2,
+        SensorsModule = 3,
+        DlKinectmodule = 4,
+    }
+}
---
+++ Modules/VoiceModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongService.Modules
+{
+    public class VoiceModule
+    {
+        public int Id { get; set; }
+
+    }
+}
