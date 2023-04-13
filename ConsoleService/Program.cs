ac2feb64301b05fbb86f50987c9719345af59056
B1a7 <93325616+B1a7@users.noreply.github.com>
2023-04-14 00:46:57 +0200

    Add files via upload
    added console service for testing purpose

---
+++ ConsoleService/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PongService;
+using System.Threading;
+
+namespace ConsoleService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var pongService = PongHTTPService.GetInstance();
+
+            Task.Run(async () =>
+            {
+                await pongService.ServerListenerAsync();
+            });
+
+
+            while (true)
+            {
+                Console.WriteLine(PongHTTPService.GetInstance().VoiceModule.Id);
+                Console.WriteLine(PongHTTPService.GetInstance().VoiceModule.RacketDirection);
+                Console.WriteLine(PongHTTPService.GetInstance().VoiceModule.Velocity);
+
+                await Task.Delay(100);
+                Console.Clear();
+            }
+        }
+    }
+}
